64483028e199fd8cf9a8784a3a83681ad79e9b59
Dan nguyen <danek.nguyen@gmail.com>
2021-03-02 16:22:43 +0100

    Hotový projekt

---
+++ ShoppingList/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ShoppingList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(433, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hotovo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nakup si:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ShoppingList/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoppingList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Item item = new Item();
+            Item.topCounter += 30;
+            item.ItemCreate(this);
+            button1.Top += 30;
+            if(!Item.controls.Contains(button1))
+            {
+                Item.controls.Add(button1);
+            }
+        }
+    }
+    public class Item
+    {
+        public static int topCounter = 0;
+        public static List<Control> controls = new List<Control>();
+        TextBox text = new TextBox();
+        Button delete = new Button();
+        CheckBox done = new CheckBox();
+        public void ItemCreate(Form Form1)
+        {
+            text.Left = 80;
+            text.Top = topCounter;
+            text.Width = 350;
+
+            done.Top = topCounter;
+            done.Left = text.Left + text.Width + 15;
+
+            delete.Top = topCounter;
+            delete.Left = done.Left + done.Width + 10;
+            delete.Text = "Smazat";
+            delete.Click += new EventHandler(delete_Click);
+
+            controls.Add(done);
+            controls.Add(text);
+            controls.Add(delete);
+
+            Form1.Controls.Add(text);
+            Form1.Controls.Add(done);
+            Form1.Controls.Add(delete);
+        }
+        public void delete_Click(object sender, EventArgs e)
+        {
+            this.text.Dispose();
+            this.done.Dispose();
+            this.delete.Dispose();
+            MoveUp(this.delete.Top);
+            topCounter -= 30;
+        }
+        public void MoveUp(int itemTop)
+        {
+            foreach(Control control in controls)
+            {
+                if(control.Top > itemTop)
+                {
+                    control.Top -= 30;
+                }
+            }
+        }
+    }
+}
